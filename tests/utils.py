9032222f08bda6d7ce28694d46ab2c51ec81ae3f
Matt Kipps <matt.kipps@gmail.com>
2019-05-31 23:25:34 -0700

    add interesting utilities
    add collection utils: group_by, index, map_keys, map_vals

--- tests/utils.py
+++ tests/utils.py
@@ -23,3 +23,57 @@ class UtilsTests(unittest.TestCase):
         # value must be hashable to become a key
         d = {"a": [1, 2]}
         self.assertRaises(TypeError, u.invert_dict, d)
+
+    def test_dict_map(self):
+        d = dict(zip("abcd", range(4)))
+        d2 = u.map_keys(lambda k: k.upper(), d)
+
+        self.assertTrue(d2 is not d)
+        self.assertEqual({"a": 0, "b": 1, "c": 2, "d": 3}, d)
+        self.assertEqual({"A": 0, "B": 1, "C": 2, "D": 3}, d2)
+
+        d3 = u.map_vals(lambda v: v + 10, d)
+        self.assertEqual({"a": 10, "b": 11, "c": 12, "d": 13}, d3)
+
+        d4 = u.map_kvs(lambda k: k + k, lambda v: v * v, d)
+        self.assertEqual({"aa": 0, "bb": 1, "cc": 4, "dd": 9}, d4)
+
+    def test_group_by(self):
+        a1 = {"a": "a1", "b": 0}
+        a2 = {"a": "a2", "b": 1}
+        a3 = {"a": "a3", "b": 2}
+        a4 = {"a": "a4", "b": 3}
+        a5 = {"a": "a5", "b": 2}
+        a6 = {"a": "a6", "b": 0}
+        coll = [a1, a2, a3, a4, a5, a6]
+
+        groups = u.group_by(lambda d: d["b"], coll)
+        self.assertEqual({0: [a1, a6], 1: [a2], 2: [a3, a5], 3: [a4]}, groups)
+
+        groups = u.group_by(lambda d: d["a"], coll)
+        self.assertEqual(
+            {"a1": [a1], "a2": [a2], "a3": [a3], "a4": [a4], "a5": [a5], "a6": [a6]},
+            groups,
+        )
+
+        groups = u.group_by(lambda d: d.get("z"), coll)
+        self.assertEqual({None: [a1, a2, a3, a4, a5, a6]}, groups)
+
+    def test_group_by_numbers(self):
+        nums = range(10)
+        groups = u.group_by(lambda x: x % 2 == 0, nums)
+        evens = groups[True]
+        odds = groups[False]
+
+        # groups are stored in standard python lists
+        self.assertTrue(isinstance(evens, list))
+        self.assertTrue(isinstance(odds, list))
+
+        self.assertEqual([0, 2, 4, 6, 8], evens)
+        self.assertEqual([1, 3, 5, 7, 9], odds)
+
+    def test_index(self):
+        nums = range(100)
+        index = u.index(lambda x: x % 2 == 0, nums)
+
+        self.assertEqual({True: 98, False: 99}, index)
--- transducers/utils.py
+++ transducers/utils.py
@@ -1,12 +1,7 @@
 from typing import Dict, Iterable
 
-from transducers.typing import Coll
+from transducers.typing import Coll, Fn
 import transducers.transducers as t
-
-
-def invert_dict(d: Dict) -> Coll:
-    invert = t.map(lambda tup: (tup[1], tup[0]))
-    return t.into_new(invert, d)
 
 
 def add(x, *xs):
@@ -31,3 +26,54 @@ def mult(x, *xs):
 
 def product(coll: Iterable):
     return t.reduce(mult, 1, coll)
+
+
+__invert_dict = t.map(lambda tup: (tup[1], tup[0]))
+
+
+def invert_dict(d: Dict) -> Coll:
+    return t.into_new(__invert_dict, d)
+
+
+def map_kvs(kfn: Fn, vfn: Fn, d: Dict):
+    xf = t.map(lambda tup: (kfn(tup[0]), vfn(tup[1])))
+    return t.into_new(xf, d)
+
+
+def map_keys(f: Fn, d: Dict):
+    return map_kvs(f, t.identity, d)
+
+
+def map_vals(f: Fn, d: Dict):
+    return map_kvs(t.identity, f, d)
+
+
+def group_by(f: Fn, coll: Iterable):
+    """
+    Group the values of `coll` in a dict using the key function `f` on each
+    value to determine the group. Returns a dict keyed by the results of `f`
+    on the values of `coll`.
+    """
+
+    def rf(acc, v):
+        k = f(v)
+        if k in acc:
+            acc[k].append(v)
+        else:
+            acc[k] = [v]
+        return acc
+
+    return t.reduce(rf, {}, coll)
+
+
+def index(f: Fn, coll: Iterable):
+    """
+    Like `group_by` but only indexes one value per key. If multiple values
+    map to the same key, indexes the latest value.
+    """
+
+    def rf(acc, v):
+        acc[f(v)] = v
+        return acc
+
+    return t.reduce(rf, {}, coll)
